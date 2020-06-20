91bd111e67195763163da1e449ad60197fc70aa5
Nitin Sai <43436336+Nitin1901@users.noreply.github.com>
2020-06-20 19:51:56 +0530

    Update processing_dataset.py

--- Mask Detection/processing_dataset.py
+++ Mask Detection/processing_dataset.py
@@ -1,5 +1,7 @@
 import cv2
 import xml.etree.ElementTree as ET
+from PIL import Image
+import numpy as np
 
 m = 0
 wm = 0
@@ -16,7 +18,7 @@ for i in range(853):
     width, height, depth = [int(i.text) for i in size]
     objects = root.findall('object')
 
-    img = cv2.imread(f'{folder}\{filename}', -1)
+    img = cv2.imread(f'{folder}\{filename}', 1).astype(np.uint8)
 
     for obj in objects:
 
@@ -28,15 +30,19 @@ for i in range(853):
         x_min, y_min, x_max, y_max = [int(i.text) for i in obj.find('bndbox')]
 
         face_img = img[y_min:y_max, x_min:x_max]
+        
+        face_img = cv2.cvtColor(face_img,cv2.COLOR_BGR2RGB)
+        pil_im = Image.fromarray(face_img)
+        pil_im = pil_im.resize((224,224), Image.LANCZOS)
 
         if name=='with_mask':
-            cv2.imwrite(f'dataset\with_mask\image{m}.png', face_img)
+            pil_im.save(f'dataset\with_mask\image{m}.png')
             m += 1
         elif name=='without_mask':
-            cv2.imwrite(f'dataset\without_mask\image{wm}.png', face_img)
+            pil_im.save(f'dataset\without_mask\image{wm}.png')
             wm += 1
         else:
-            cv2.imwrite(f'dataset\incorrect_mask\image{im}.png', face_img)
+            pil_im.save(f'dataset\incorrect_mask\image{im}.png')
             im += 1
 
 print(m, wm, im)
